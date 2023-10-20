2b43976798841ff8ab8a45b14ff59da5f7b8a9fb
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-10-20 16:37:06 -0500

    create  screens login

--- src/components/user/Login/LoginComponents.jsx
+++ src/components/user/Login/LoginComponents.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Image, Text, View } from 'react-native'; // Importa Image, Text y View de 'react-native'
-import ImageLogin from '../../../../image/ImageLogin/ImageLogin.png'; 
+import React from "react";
+import { Image, TextInput, View, Text } from "react-native"; // Importa Image, Text y View de 'react-native'
+import ImageLogin from "../../../../image/ImageLogin/ImageLogin.png";
 import styles from "./LoginComponentsStyle"; // Importa los estilos desde el archivo correcto
 
 export const LoginComponents = () => {
   return (
     <View style={styles.container}>
-      
-      <Image 
-        source={ImageLogin}
-        style={styles.image}
+      <Image source={ImageLogin} style={styles.image} />
+      <TextInput
+        placeholder="Corredo"
+        keyboardType="email-address"
+        type="email"
       />
+      <TextInput placeholder="Password" type="password" />
+
+      <Text>
+        ¿Olvidaste tu Contraseña?{"  "}
+        <Text>Recuperala</Text>
+      </Text>
     </View>
   );
-}
+};
