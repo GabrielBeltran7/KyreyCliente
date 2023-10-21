aecaa9ceec283fd3a44561f95c934ebbdb36dc06
AngelSilva98 <angelceesd@gmail.com>
2023-10-21 20:11:28 -0300

    estilos login

--- src/components/user/Login/LoginComponentsStyle.jsx
+++ src/components/user/Login/LoginComponentsStyle.jsx
@@ -2,78 +2,55 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   outerContainer: {
-    flex: 1,
-    backgroundColor: "#F25B06", // Fondo naranja que cubre toda la pantalla
-    minHeight: "calc(100vh - 64px)",
+    backgroundColor: "#F25B06",
+    minHeight: 1500,
   },
   container: {
-    flex: 1,
-    // Cambia el color de fondo según tus preferencias
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    padding: 15,
+    marginTop: 10,
   },
   image: {
-    borderRadius: 15,
-    width: 150, // Ajusta el tamaño de la imagen
-    height: 150, // Ajusta el tamaño de la imagen
-    marginBottom: 20, // Espacio entre la imagen y los campos de entrada
+    width: 200,
+    height: 200,
+    color: "#ffe500",
   },
   inputContainer: {
-    width: "80%", // Ajusta el ancho del contenedor de entrada
-    marginBottom: 15, // Espacio entre contenedores de entrada
-  },
-  input: {
-    width: "100%",
-    backgroundColor: "white",
-    height: 50, // Altura de los campos de entrada
-    borderWidth: 1, // Borde para los campos de entrada
-    borderColor: "#ccc", // Color del borde
-    borderRadius: 5, // Radio de borde
-    paddingHorizontal: 10, // Relleno horizontal
-    marginBottom: 10, // Espacio entre campos de entrada
-  },
-  forgotPasswordText: {
-    padding: 10,
-    color: "white", // Color del texto
-    fontSize: 15,
-  },
-  button: {
-    color: "#007PHF",
-    backgroundColor: "black",
-    padding: 15,
-    width: "80%",
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  textbotton: {
-    color: "white",
-    fontSize: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   iconInputContainer: {
-    flexDirection: "row", // Para colocar los componentes en fila
-    alignItems: "center", // Alinea verticalmente los componentes
-  },
-  inputIcon: {
-    marginRight: 0,
-    color: "black", // Espacio entre el ícono y el campo de entrada
-    backgroundColor: "white",
-    height: 20,
-    width: 20,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
+    gap: 5,
   },
-  tamaño: {
-    marginRight: 0,
-    color: "black", // Espacio entre el ícono y el campo de entrada
-    backgroundColor: "white",
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    width: "80%",
     height: 50,
-    width: 25,
-    alignItems: "center",
+    fontSize: 15,
+  },
+  forgotPasswordText: {
+    color: "white",
+  },
+  button: {
+    backgroundColor: "#ffe500",
+    margin: 10,
+    width: "50%",
+    padding: 10,
+    borderRadius: 20,
+  },
+  textbotton: {
+    textAlign: "center",
+    borderRadius: 10,
   },
   errorText: {
-    color: "white",
-    alignContent: "center",
+    color: "#f7ea62",
   },
 });
 
